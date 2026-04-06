7e38ad0b63813c0bb71304c51839954c63da2e38
blueowlcleaning <blueowlcleaninguk@gmail.com>
2026-04-06 15:37:17 +0000

    convert to commonjs

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -25,4 +25,4 @@ export default async function handler(req, res) {
     console.error('Checkout error:', error.message);
     return res.status(500).json({ error: error.message });
   }
-}
+};
